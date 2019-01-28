f234cfda182d7c4ece136c86b781e38a3e72aee6
Snguyen2 <42800653+Snguyen2@users.noreply.github.com>
2019-01-28 10:41:56 +0100

    php

--- h06/school.php
+++ h06/school.php
@@ -6,16 +6,17 @@
  * Time: 09:58
  */
 
-$user = 'schooluser';
-$pass = 'Sang12345';
-
-$dbh = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port, $user, $pass);
+$user = "schooluser";
+$pass = "Sang12345";
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=school;port=3306', $user, $pass);
-    foreach ($dbh->query('SELECT * from cursist') as $row) {
-        }
-    $dbh = null;
- }   catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
+    foreach($dbh->query('SELECT * from cursist') as $row) {
+        print_r($row);
     }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
